54928d2719d2508f5379203946958a1f27334fe4
RodPy <rodneyrrspy@gmail.com>
2022-04-26 16:41:27 -0400

    FK script

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ humedad= sense.humidity
 print(temperatura, humedad)
 
 
-conn = sqlite3.connect('Desktop/BD/baseAbril.db')
+conn = sqlite3.connect('/home/pi/Desktop/BD/baseAbril.db')
 
 print ("LISTO")
 
 cur =conn.cursor()
-cur.execute("CREATE TABLE Nodo2(N integer PRIMARY KEY AUTOINCREMENT, datatime integer, Tempertatura real,humedad real)")
+# cur.execute("CREATE TABLE Nodo2(N integer PRIMARY KEY AUTOINCREMENT, datatime integer, Tempertatura real,humedad real)")
 
 ##lecturas=(1,4)
-cur.execute("INSERT INTO Nodo2(datatime,Tempertatura,humedad) VALUES(datetime('now','localtime'),?,?)",(temperatura,humedad))
-conn.commit()
+while True:
+    temperatura = sense.temperature
+    humedad= sense.humidity
+    cur.execute("INSERT INTO Nodo2(datatime,Tempertatura,humedad) VALUES(datetime('now','localtime'),?,?)",(temperatura,humedad))
+    conn.commit()
